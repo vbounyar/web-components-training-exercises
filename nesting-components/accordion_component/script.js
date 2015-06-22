07b1b5121ff57824f57a257dd05208b3d123ec75
vbounyar <vbounyar@gmail.com>
2015-06-22 14:11:40 -0700

    Added Hello Badge into the Style Guide.

--- nesting-components/accordion_component/script.js
+++ nesting-components/accordion_component/script.js
@@ -1,15 +1,36 @@
 var accordions = Array.prototype.slice.call(document.querySelectorAll('.accordion-component'));
+
+
+
 accordions.forEach(function(accordion) {
+
+  // Made into arrays from the notes
   var allContent = Array.prototype.slice.call(accordion.querySelectorAll('.content'));
+  
   var sections = Array.prototype.slice.call(accordion.querySelectorAll('.section'));
+  
+  
   sections.forEach(function(section) {
-    var content = section.querySelector('.content');
-    section.querySelector('h2').addEventListener('click', function() {
-      var closed = content.classList.contains('closed');
-      allContent.forEach(function(content) {
-        content.classList.add('closed');
-      });
-      closed ? content.classList.remove('closed') : null;
-    });
+    var contents = Array.prototype.slice.call(section.querySelectorAll('.content'));
+	/*section.querySelector('.content');*/
+	
+
+	  section.querySelector('h2').addEventListener('click', function() {
+	  
+
+	  var closed = content.classList.contains('closed');
+	  
+	  allContent.forEach(function(con) {
+			con.classList.add('closed');
+		
+	  });
+	  
+	  closed ? content.classList.remove('closed') : null;
+
+	 
+	});
+	
   });
+  
 });
+
